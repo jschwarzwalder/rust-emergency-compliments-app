8f77ed9f46f392543510ac3181091b03104af470
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 15:20:39 -0700

    Adding support for handlebars

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate simple_server;
 use simple_server::Server;
 use motivations::MOTIVATIONS;
 use pick_one::pick_one_str;
+use handlebars::Handlebars;
+use std::collections::BTreeMap;
 
 fn main() {
     const template: &str = r#"<!DOCTYPE html>
@@ -32,18 +34,15 @@ fn main() {
         </html>"#;
     let server = Server::new(|request, mut response| {
         let mut new_template = template.to_string();
-        let mut crab_nums = vec![];
-        for i in 1..13 {
-            crab_nums.push(i.to_string());
-        }
-        let mut new_crab_nums = vec![];
-        for crab_str_num in crab_nums.iter() {
-            new_crab_nums.push(&crab_str_num[..]);
-        }
-       
-        new_template = new_template.replace("{{ num }}", pick_one_str(&new_crab_nums[..]));
-        new_template = new_template.replace("{{ motivation }}", pick_one_str(&MOTIVATIONS));
-        Ok(response.body(new_template.as_bytes().to_vec())?)
+        let mut handlebars = Handlebars::new();
+        let crab_nums = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+        let mut data = BTreeMap::new();
+        data.insert("num", pick_one_str(&crab_nums[..]));
+        data.insert("motivation", pick_one_str(&MOTIVATIONS));
+        Ok(response.body(handlebars.render_template(&new_template, &data)
+            .expect("expect String")
+            .as_bytes()
+            .to_vec())?)
     });
 
     
