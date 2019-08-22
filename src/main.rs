5e0e09b3e08983d075a89043ccda127bacd3c935
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 14:06:11 -0700

    making a server page locally

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,38 @@ extern crate simple_server;
 use simple_server::Server;
 
 fn main() {
+    const template: &str = r#"<!DOCTYPE html>
+        <html>
+        <head>
+            <title>RustTogether</title>
+            <link rel="stylesheet" href="styles.css"/>
+            <style>
+            body {
+                background: url('img/crab-13.jpg'); 
+                background-size: cover;
+            }
+            </style>
+        </head>
+        <body>
+            <header>
+            <span class="logo">RustTogether</span>
+            </header>
+            <div class="container">
+            <h1>
+                <div class="motivation">
+                hey girl
+                </div>
+            </h1>
+            </div>
+        </body>
+        </html>"#;
     let server = Server::new(|request, mut response| {
-        Ok(response.body("Hello, world!".as_bytes().to_vec())?)
+        Ok(response.body(template.as_bytes().to_vec())?)
     });
 
+    
+
     server.listen("localhost", "4000");
+
+    
 }
