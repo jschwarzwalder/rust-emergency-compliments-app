8abe6929ab8e75ab4e859127fd26af81baa6d95d
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 14:47:33 -0700

    randomizing crabs and messages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate simple_server;
 
 use simple_server::Server;
 use motivations::MOTIVATIONS;
+use pick_one::pick_one_str;
 
 fn main() {
     const template: &str = r#"<!DOCTYPE html>
@@ -11,7 +12,7 @@ fn main() {
             <link rel="stylesheet" href="styles.css"/>
             <style>
             body {
-                background: url('img/crab-{{image}}.jpg'); 
+                background: url('img/crab-{{ num }}.jpg'); 
                 background-size: cover;
             }
             </style>
@@ -30,7 +31,18 @@ fn main() {
         </body>
         </html>"#;
     let server = Server::new(|request, mut response| {
-        let mut new_template = MOTIVATIONS[0];
+        let mut new_template = template.to_string();
+        let mut crab_nums = vec![];
+        for i in 1..13 {
+            crab_nums.push(i.to_string());
+        }
+        let mut new_crab_nums = vec![];
+        for crab_str_num in crab_nums.iter() {
+            new_crab_nums.push(&crab_str_num[..]);
+        }
+       
+        new_template = new_template.replace("{{ num }}", pick_one_str(&new_crab_nums[..]));
+        new_template = new_template.replace("{{ motivation }}", pick_one_str(&MOTIVATIONS));
         Ok(response.body(new_template.as_bytes().to_vec())?)
     });
 
